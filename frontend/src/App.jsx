b0f04ece2d6d77f6cd0971aafb8cbd45894b8047
Ritesh Maurya <riteshkumar411552@gmail.com>
2025-07-07 16:24:29 +0530

    Create Pages and Routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,32 @@
 
 
 import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import Collection from './pages/Collection'
+import About from './pages/About'
+import Login from './pages/Login'
+import Contact from './pages/Contact'
+import Orders from './pages/Orders'
+import PlaceOrder from './pages/PlaceOrder'
+import Cart from './pages/Cart'
+import Product from './pages/Product'
 
 const App = () => {
   return (
-    <div>App</div>
+    <div className='px-4 sm:px-[5vw] md:px-[7vw] lg-px-[9vw]'>
+    <Routes>
+      <Route path='/' element={<Home/>}/>
+      <Route path='/collection' element={<Collection/>}/>
+      <Route path='/about' element={<About/>}/>
+      <Route path='/login' element={<Login/>}/>
+      <Route path='/contact' element={<Contact/>}/> 
+      <Route path='/orders' element={<Orders/>}/> 
+      <Route path='/place-order' element={<PlaceOrder/>}/>
+      <Route path='/cart' element={<Cart/>}/>
+      <Route path='/product/:productId' element={<Product/>}/>
+    </Routes>
+    </div>
   )
 }
 
