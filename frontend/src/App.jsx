f61376a14b573899e4fce59d8573276a2417d074
Ritesh Maurya <riteshkumar411552@gmail.com>
2025-07-08 13:45:44 +0530

    Add Footer component and integrate it into App; update App layout

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,35 @@
-
-
-import React from 'react'
-import { Routes, Route } from 'react-router-dom'
-import Home from './pages/Home'
-import Collection from './pages/Collection'
-import About from './pages/About'
-import Login from './pages/Login'
-import Contact from './pages/Contact'
-import Orders from './pages/Orders'
-import PlaceOrder from './pages/PlaceOrder'
-import Cart from './pages/Cart'
-import Product from './pages/Product'
-import Navbar from './components/Navbar'
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Collection from "./pages/Collection";
+import About from "./pages/About";
+import Login from "./pages/Login";
+import Contact from "./pages/Contact";
+import Orders from "./pages/Orders";
+import PlaceOrder from "./pages/PlaceOrder";
+import Cart from "./pages/Cart";
+import Product from "./pages/Product";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
 const App = () => {
   return (
-    <div className='px-4 sm:px-[5vw] md:px-[7vw] lg-px-[9vw]'>
-     <Navbar/> 
-    <Routes>
-      <Route path='/' element={<Home/>}/>
-      <Route path='/collection' element={<Collection/>}/>
-      <Route path='/about' element={<About/>}/>
-      <Route path='/login' element={<Login/>}/>
-      <Route path='/contact' element={<Contact/>}/> 
-      <Route path='/orders' element={<Orders/>}/> 
-      <Route path='/place-order' element={<PlaceOrder/>}/>
-      <Route path='/cart' element={<Cart/>}/>
-      <Route path='/product/:productId' element={<Product/>}/>
-    </Routes>
+    <div className="px-4 sm:px-[5vw] md:px-[7vw] lg-px-[9vw]">
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/collection" element={<Collection />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/orders" element={<Orders />} />
+        <Route path="/place-order" element={<PlaceOrder />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/product/:productId" element={<Product />} />
+      </Routes>
+      <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
