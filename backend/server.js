ab0a17ecad4671eb8e3d6d44f029e7dcf5e89bbe
Ritesh Maurya <riteshkumar411552@gmail.com>
2025-07-11 17:43:35 +0530

    Initialize backend with Express server and essential dependencies

---
+++ backend/server.js
@@ -0,0 +1,21 @@
+import express from "express";
+import cors from "cors";
+import "dotenv/config";
+
+// App config
+const app = express();
+const PORT = process.env.PORT || 8000;
+
+// Middlewares
+app.use(express.json());
+app.use(cors());
+
+// api endpoint
+
+app.get("/", (req, res) => {
+  res.send("Api working");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port:${PORT}`);
+});
