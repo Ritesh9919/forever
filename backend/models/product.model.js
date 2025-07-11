4e9ae2966b5361115b396b2e65bf8c3bfe01aa12
Ritesh Maurya <riteshkumar411552@gmail.com>
2025-07-11 18:03:59 +0530

    Add product model with schema definition for MongoDB

---
+++ backend/models/product.model.js
@@ -0,0 +1,45 @@
+import mongoose from "mongoose";
+
+const productSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: String,
+      required: true,
+    },
+    image: {
+      type: Array,
+      required: true,
+    },
+    category: {
+      type: String,
+      required: true,
+    },
+    subCategory: {
+      type: String,
+      required: true,
+    },
+    size: {
+      type: Array,
+      required: true,
+    },
+    bestSeller: {
+      type: Boolean,
+    },
+    date: {
+      type: Number,
+      required: true,
+    },
+  },
+  { timeseries: true }
+);
+
+export const Product =
+  mongoose.models.product || mongoose.model("Product", productSchema);
