18e89b77a14e1ff42efeff2f9db69ddc43fe564e
Ritesh Maurya <riteshkumar411552@gmail.com>
2025-07-14 10:23:04 +0530

    feat: implement admin panel layout with routing and authentication

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,8 +1,35 @@
-import React from "react";
+import Navbar from "./components/Navbar";
+import Sidebar from "./components/Sidebar";
+import { Routes, Route } from "react-router-dom";
+import Add from "./pages/Add";
+import List from "./pages/List";
+import Orders from "./pages/Orders";
+import { useState } from "react";
+import Login from "./components/Login";
 
 const App = () => {
+  const [token, setToken] = useState("m");
   return (
-    <div className="text-2xl text-green-600 text-center mt-20">Hello admin</div>
+    <div className="bg-gray-50 min-h-screen">
+      {token ? (
+        <Login />
+      ) : (
+        <>
+          <Navbar />
+          <hr />
+          <div className="flex w-full">
+            <Sidebar />
+            <div className="w-[70%] mx-auto ml-[max(5vw, 25px)] my-8 text-gray-600 text-base">
+              <Routes>
+                <Route path="/add" element={<Add />} />
+                <Route path="/list" element={<List />} />
+                <Route path="/orders" element={<Orders />} />
+              </Routes>
+            </div>
+          </div>
+        </>
+      )}
+    </div>
   );
 };
 
